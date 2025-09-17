c390cb66240f61bc90fdfd54d841b3f22679d331
Tormod Iversen <tormod.iversen@enoro.com>
2025-09-17 06:03:48 +0000

    modified an error message

--- src/app.py
+++ src/app.py
@@ -64,7 +64,7 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Validate if a student is already signed up
     if email in activity["participants"]:
-        raise HTTPException(status_code=400, detail="Student already signed up for this activity")
+        raise HTTPException(status_code=400, detail="Student already signed up for this activity. Can't be there twice!")
 
     # Add student
     activity["participants"].append(email)
